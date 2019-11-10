c57b8255515b6067f971de866cc46c053fe04268
Helder Pinto <hybridhp@hotmail.com>
2019-11-10 03:42:57 +0100

    first commit

---
+++ Katerina_Tasks1.playground/Contents.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+
+/* 1 - Define an array with the following items 'apple','grape', 'bacon', 'steak', 'worm', 'dirt'.
+    Assign the array to a variable called food. Loop through the array and print 'fruit' if the outcome is fruit,
+    print 'meat' if the outcome is meat otherwise print 'yuck'.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 2- Given the array sounds, write code that loops over the array and adds all the strings together to form one large combined string without space between them.
+  HINT: Save the result in a variable called finalString.
+ */
+
+var sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 3 -  Loop through number between 20 and 40  and if the number is even print 'number x is even'
+ if the number is odd print 'number x is odd'.
+ Note: x should be the number in case
+ */
+
+
+
+
+
+
+
+
+
+
+
+/* 4 - Loop through the ages array and print 'at age x you are old enough to drink' if the age is between 18 and 65 inclusive, if the age is until 17 print 'at age x you are not old enough to drink' and if the age is more than 65 print 'at age x you are too old to drink'
+
+ Note: x should be sustituted by the age while looping
+*/
+
+var ages = [12, 34, 65, 18, 17, 67, 25]
+
+
+
+
+
+
+
+
+
+
+
+
+/* 5 - Loop through numbers array and print the amount of even and the amount of odd numbers that the array contains.
+ Hint: save the results in two varibles, one for even and another for odd numbers
+ */
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
---
+++ Marketa_Tasks1.playground/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+/* 1 - Print fizzBuzz  when there is a multiple of 3 and/ or 5 for numbers up to 50. */
+
+
+
+
+
+
+
+
+
+
+/* 2 - Print the following pattern
+
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+
+ */
+
+
+
+
+
+
+
+
+
+/* 3 - Sum the multiples of 3 and 5 up to 1000. */
+
+
+
+
+
+
+
+/* 4 - Given the following array, multiply every second value*/
+
+
+var multiplyEveryOther = [1,2,3,4,5,6,7,8]  // the outcome should be 2x4x6x8
+
+
+
+
+
+
+
+
+
+/* 5 - Given the array sumPairs, return the first pair of numbers that sum to 12 */
+
+var sumPairs = [2,4,5,7,5,2,10]      // outcome should be [5, 7]
+
+
+
+
+
