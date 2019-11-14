4b6d6db8f143de344a02bdacff46cd96b9339cc1
Helder Pinto <hybridhp@hotmail.com>
2019-11-14 15:34:09 +0100

    tasks 2

---
+++ Katerina_task2.playground/Contents.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+/* 1 - Define a function that takes a string and prints the string. Call the function*/
+
+
+
+
+
+
+
+/* 2 - Define a function that takes a string, loops through the string and prints each letter from the string. Call the function*/
+
+
+
+
+
+
+
+
+/* 3 - Define a function that takes a string, loops through the string and prints only the letter 'a' from the string . Call the function
+
+
+    example: if the string is "joana" the function will print the letter 'a' twice
+
+ */
+
+
+
+
+
+
+
+
+
+/* 4 - Define a function that takes an integer and prints the integer multiplied by 5*/
+
+
+
+
+
+
+
+
+/* 5 - Define a function that takes an array of integers and prints the array*/
+
+
+
+
+
+
+
+
+
+/* 6 -  Define a function that takes an array of stringd and prints the array*/
+
+
+
+
+
+
+/* 7 - Define a function that takes an array of integers, loops through the array and returns all numbers from the array */
+
+
+
+
+
+
+
+
+/* 8 - Define a function that takes an array of integers, loops through the array and returns all numbers multiplied by 5 */
---
+++ Marketa_Tasks2.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+/*1 -  write a function called same which accepts two arrays and returns TRUE if every value in the array has its corresponding value squared in the second array. The Frequency of Values must be the same.
+                Outcome:  same([1,2,3], [4,1,9])  returns true
+                          same([1,2,3], [1,9])  return false
+ */
+
+
+
+
+
+
+
+/*
+2 - Write function that takes two string and return true if the second is an anagram of the first.
+    Anagram is word phrase or sequence formed by the same letters/symbols/numbers from another.
+    example: Dormitory = Dirty room.
+             School master = The classroom
+             Listen = Silent
+             Astronomer = Moon starer
+             The eyes = They see
+
+     Outcome: validAnagram('', '')  returns true
+              validAnagram('Listen', 'Silent') returns true
+              validAnagram('bar', 'car') returns false
+
+ */
+
+
+
+
+
+/* 3 - Write a function that takes a sorted array on Integers and returns how many unique values are in the array.
+
+    Outcome: uniqueValues([2,2,2,2,3,3,5,6]) returns 4
+             uniqueValues([1,1,2,2]) returns 2
+ */
+
+
+
+
+
+/* 4 - Write a function which accepts an array of integers and a second argument called num of type Int, and returns the maximum sum of  num consecutive elements in the array.
+
+        Outcome: subSum([1,2,3,4], 2)    returns 7     because 3+4
+                 subSum([10,2,3,5,20,3], 3)       returns 28      because 5+20+3
+ */
+
+
+
+
